0ae7058d34fb741b6336010dba7fc615b7a2618f
boeschy <m.boesch@outlook.de>
2020-09-21 10:56:53 +0200

    fix missing includes

--- libavcodec/v4l2_request.c
+++ libavcodec/v4l2_request.c
@@ -21,7 +21,9 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>      
 #include <fcntl.h>
+#include <unistd.h>
 
 #include <sys/sysmacros.h>
 #include <libudev.h>
